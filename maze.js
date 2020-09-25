f6dea9d0f349407267c47a7ff20b13f99dd300ae
just2click <just2click@outlook.com>
2020-09-25 22:56:09 +0300

    Initial

---
+++ maze.js
@@ -0,0 +1,31 @@
+/**
+ * We have a maze, a matrix M x N
+ * Start point - s
+ * End point - e
+ * Can pass - 0
+ * Cannot pass - 1
+ *
+ * s 1 1 1 1
+ * 0 0 0 0 1
+ * 1 1 1 0 1
+ * 1 1 1 e 1
+ * 1 1 1 1 1
+ *
+ * s 1 1 1 1
+ * 0 1 0 0 1
+ * 1 1 1 0 1
+ * 1 1 1 e 1
+ * 1 1 1 1 1
+ *
+ * write a function to solve this maze
+ */
+
+ /**
+  *
+  * @param {Start} s
+  * @param {End} e
+  * @param {Maze} m
+  */
+ function solve (s, e, m) {
+
+ }
